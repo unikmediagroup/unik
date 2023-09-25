b161e420c881e46cfb2936505eb0c3f962a4a796
abyabraham93 <abyabraham93@gmail.com>
2023-09-26 04:36:24 +0530

    Update everflowLoader.js

--- everflowLoader.js
+++ everflowLoader.js
@@ -10,21 +10,6 @@
     document.head.appendChild(pymScript);
 
     window.loadIframe = function(everflowURL) {
-        // Check if everflowURL already has parameters
-        if (everflowURL.indexOf('?') !== -1) {
-            // If it does, replace the '?' in window.location.search with '&'
-            var searchParams = new URLSearchParams(window.location.search);
-            searchParams.delete('uid');
-            searchParams.delete('oid');
-            everflowURL += '&' + searchParams.toString();
-        } else {
-            var searchParams = new URLSearchParams(window.location.search);
-            searchParams.delete('uid');
-            searchParams.delete('oid');
-            everflowURL += '?' + searchParams.toString();
-        }
-
-        // Load the iframe with the modified URL
         var pymParent = new pym.Parent('pym-container', everflowURL, {});
     };
 })();
