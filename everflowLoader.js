b930394a449c98dc971380791665ad74eebc24f2
abyabraham93 <abyabraham93@gmail.com>
2023-09-28 02:03:22 +0530

    Update everflowLoader.js

--- everflowLoader.js
+++ everflowLoader.js
@@ -1,10 +1,24 @@
-// Load the pym library dynamically
 (function() {
     var pymScript = document.createElement('script');
-    pymScript.src = 'https://pym.nprapps.org/pym.v1.min.js';
+    pymScript.src = 'https://cdn.jsdelivr.net/gh/unikmediagroup/unik@main/pym.v1.min.js';
     pymScript.onload = function() {
         if (window.everflowURL) {
-            loadIframe(window.everflowURL);
+            // Capture the specified URL parameters from the parent page
+            var params = ['fbclid', 'gclid', 'sub1', 'sub2', 'sub3', 'sub4', 'sub5', 'source_id'];
+            var queryParams = new URLSearchParams(window.location.search);
+            var newParams = new URLSearchParams();
+            params.forEach(function(param) {
+                if (queryParams.has(param)) {
+                    newParams.set(param, queryParams.get(param));
+                }
+            });
+
+            // Append the captured parameters to the everflowURL
+            var everflowURL = new URL(window.everflowURL);
+            everflowURL.search ? newParams.forEach((value, key) => everflowURL.searchParams.append(key, value)) : everflowURL.search = newParams.toString();
+
+            // Load the iframe with the modified URL
+            loadIframe(everflowURL.toString());
         }
     };
     document.head.appendChild(pymScript);
